695b6ddde81674f33f00fe3d3c8d6315563d990a
Jarod Reyes <jarodreyes@gmail.com>
2016-02-18 09:53:36 -0800

    Fixed the tutorial references.

---
+++ src/main/java/com/twilio%A0calltracking/models/Lead.java
@@ -0,0 +1,66 @@
+package com.twilio.calltracking.models;
+
+import javax.persistence.*;
+
+@SuppressWarnings("unused") @Entity @Table(name = "leads") public class Lead {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id") private long id;
+
+    @Column(name = "phone_number") private String phoneNumber;
+
+    @Column(name = "city") private String city;
+
+    @Column(name = "state") private String state;
+
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "lead_source_id") private LeadSource
+        leadSource;
+
+    public Lead() {
+    }
+
+    public Lead(String phoneNumber, String city, String state, LeadSource leadSource) {
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.state = state;
+        this.leadSource = leadSource;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LeadSource getLeadSource() {
+        return leadSource;
+    }
+
+    public void setLeadSource(LeadSource leadSource) {
+        this.leadSource = leadSource;
+    }
+}
